c85d15cd6728b9f1050d2090cc6ac160a71fec21
Stephen Riley <jstephen.riley@yahoo.com>
2019-10-07 23:25:14 -0700

    Set lang to C# 8 and turn on nullable features

--- Stuff/RequiredFromQueryAttribute.cs
+++ Stuff/RequiredFromQueryAttribute.cs
@@ -7,14 +7,13 @@ namespace RouteTest1.Stuff
 {
     public sealed class RequiredFromQueryAttribute : FromQueryAttribute, IActionConstraint, IParameterModelConvention
     {
-        private string parameterName;
+        private string? parameterName;
 
         public void Apply(ParameterModel parameter)
         {
             if (parameter.Action.Selectors != null && parameter.Action.Selectors.Any())
             {
                 parameterName = parameter.BindingInfo?.BinderModelName ?? parameter.ParameterName;
-
                 parameter.Action.Selectors.Last().ActionConstraints.Add(this);
             }
         }
